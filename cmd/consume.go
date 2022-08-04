e4ab57437f2cc6cb2ca8a8e8128a0599b387fae0
ditsuke <ditsuke@protonmail.com>
2022-08-04 10:30:55 +0530

    feat: Add flags to suppress logging to stdout

--- cmd/consume.go
+++ cmd/consume.go
@@ -4,23 +4,29 @@ import (
 	"flag"
 	ps "github.com/ditsuke/kafka-pubsub"
 	. "github.com/ditsuke/kafka-pubsub/cmd/internal"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
 	var cfg ps.SubOptions
+	var suppressLogs bool
 
 	flags := flag.NewFlagSet("", flag.ExitOnError)
 	flags.StringVar(&cfg.KafkaHost, "kafka-host", DefaultKafkaHost, "kafka host")
 	flags.IntVar(&cfg.KafkaPort, "kafka-port", DefaultKafkaPort, "kafka port")
 	flags.StringVar(&cfg.Topic, "topic", DefaultTopic, "topic to consume")
 	flags.StringVar(&cfg.Group, "group", DefaultConsumerGroup, "consumer group to use for consumption")
+	flags.BoolVar(&suppressLogs, "no-logs", false, "no logging to stdout")
 	err := flags.Parse(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	if suppressLogs {
+		log.SetOutput(ioutil.Discard)
+	}
 	log.Printf("configured: %+v\n", cfg)
 	ps.ReadFromKafka(cfg)
 }
--- cmd/publish.go
+++ cmd/publish.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	ps "github.com/ditsuke/kafka-pubsub"
 	. "github.com/ditsuke/kafka-pubsub/cmd/internal"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
 	var cfg ps.PubOptions
+	var suppressLogs bool
+
 	flags := flag.NewFlagSet("", flag.ExitOnError)
 	flags.StringVar(&cfg.KafkaHost, "kafka-host", DefaultKafkaHost, "kafka host")
 	flags.IntVar(&cfg.KafkaPort, "kafka-port", DefaultKafkaPort, "kafka port")
@@ -17,11 +20,16 @@ func main() {
 	flags.IntVar(&cfg.Partitions, "partitions", DefaultPartitions, "number of partitions")
 	flags.IntVar(&cfg.EventCount, "events", DefaultEventCount, "number of events to write")
 	flags.IntVar(&cfg.BatchSize, "batch-size", DefaultBatchSize, "number of events to write in a batch")
+	flags.BoolVar(&suppressLogs, "no-logs", false, "no logging to stdout")
+
 	err := flags.Parse(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	if suppressLogs {
+		log.SetOutput(ioutil.Discard)
+	}
 	log.Printf("configured: %+v\n", cfg)
 	ps.WriteToKafka(cfg)
 }
